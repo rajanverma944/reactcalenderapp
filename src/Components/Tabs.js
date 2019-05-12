ec49684aeb4e28888ba54cdc52c8d4098ec0f1a8
rajan verma <shivanshawesome31@gmail.com>
2019-05-12 11:29:31 +0700

    used material ui to create a structure to display data

---
+++ src/Components/Tabs.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+class MyTabs extends Component {
+    state = {
+        value: 0
+    }
+
+    handleChange = (event, newValue) => {
+        this.setState({ value: newValue }, () => this.props.switchContent(newValue));
+    }
+
+    render() {
+        return (
+            <Paper>
+                <Tabs
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    centered
+                    variant={"fullWidth"}
+                >
+                    <Tab label="Upcoming Holidays" />
+                    <Tab label="Passed Holidays" />
+                </Tabs>
+            </Paper>
+        )
+    }
+}
+
+export default MyTabs;
